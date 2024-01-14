313d64b47781443d8cbc00b318d5a1514f7bba9c
sotacode <sotacodeprivate@gmail.com>
2024-01-14 20:23:27 +1300

    orthography base done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GptModule } from './gpt/gpt.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    GptModule,
+  ]
 })
 export class AppModule {}
---
+++ src/gpt/use-cases/orthography.us-cases.ts
@@ -0,0 +1,42 @@
+import OpenAI from "openai";
+
+interface Options {
+    prompt: string;
+}
+
+export const ortographyCheckUseCases = async (openai: OpenAI, options: Options) => {
+    const { prompt } = options;
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: `
+                You are a spelling corrector; You should be able to detect the languag; what I need you to do is generate a JSON with the following structure:
+                example input prompt: "i don't have any opcionn about that quesion."
+                example output:
+                    {
+                        "total_corrections": 2, //number
+                        "corrected_text": "i don't have any |option| about that |question|." //string with the corrections applied and the corrections surrounded by | |
+                    }
+                `
+            },
+            {
+                role: "user",
+                content: prompt
+            }
+        ],
+        model: "gpt-3.5-turbo-1106",
+        temperature: 0.3,
+        max_tokens: 250,
+        response_format: {
+            type: "json_object",
+        }
+    });
+
+    console.log(completion.choices[0].message);
+    const response = JSON.parse(completion.choices[0].message.content);
+    return {
+        prompt: prompt,
+        completion: response
+    };
+}
